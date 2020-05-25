a7bcf18db51f8314225df806a16ed61cec961653
Shaneen Vitug <shaneenvitug@gmail.com>
2020-05-25 16:57:19 +1000

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       Order form coming soon!
     </div>
   );
